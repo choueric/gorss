3977b61837032b3edf2f50e4cff25a4212ec0f4f
choueric <zhssmail@gmail.com>
2015-08-26 16:53:18 +0200

    add function of fetching and parsing.
    but still need to run on GAE.

---
+++ gorss_test.go
@@ -0,0 +1,31 @@
+package gorss_test
+
+import (
+	"fmt"
+	"github.com/choueric/gorss"
+	//"io/ioutil"
+	"os"
+	"testing"
+)
+
+func Test_GenerateFeed(t *testing.T) {
+	fmt.Printf("test GenerateFeed\n")
+	gorss.GenerateFeed(os.Stdout, "zhi_japan")
+}
+
+/*
+func Test_ParsePage(t *testing.T) {
+	fmt.Printf("test ParsePage\n")
+	f, err := os.OpenFile("demo.html", os.O_RDWR, os.ModePerm)
+	if err != nil {
+		t.Errorf("open failed")
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Errorf("read failed")
+	}
+	gorss.ParsePage(data)
+}
+*/

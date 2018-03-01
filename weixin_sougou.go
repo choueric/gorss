3cf023101aa7f9d617a1222a793a4b0526013af8
choueric <zhssmail@gmail.com>
2018-03-01 14:10:30 +0800

    remove useless input.go

--- weixin_sougou.go
+++ weixin_sougou.go
@@ -52,6 +52,28 @@ type DisplayXml struct {
 	Update  string `xml:"lastModified"`
 }
 
+// RSS query info for a gzh. gzh is the "public account"
+type QueryElement struct {
+	id     string
+	name   string
+	openid string
+	eqs    string
+	cb     string // fixed
+	ekv    string // fixed
+	page   string // fixed
+	t      string
+}
+
+// add new RSS here
+var IDQuerys = []*QueryElement{
+	&QueryElement{
+		id:     "zhi_japan",
+		name:   "知日",
+		openid: "oIWsFt3YfRKPuRZmMDZAdlPJgIPU",
+		eqs:    "vVszo3Bguw%2BpoUyfUb7gSu7N7CSPLLzqm1DpF5tvTnfaP1JKRtX%2BIxaW3PH%2BFZuKmHrTW",
+	},
+}
+
 // for printf %v
 func (p *DisplayXml) String() string {
 	return fmt.Sprintf("  Title: %s\n  URL: %s\n  Content: %s\n  Date: %s\n",
